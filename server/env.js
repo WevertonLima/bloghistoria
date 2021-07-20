b46c8274848e20ddca3d4a51935e1f81bdc729d5
Weverton Lima <wevertonlima.wl@gmail.com>
2021-07-20 15:33:54 -0300

    teste prod

--- server/env.js
+++ server/env.js
@@ -22,12 +22,40 @@ var config = {
         // },
         // googleID: "629679185123-d8q71lomop2gukdgtqp2klb3atujmlnd.apps.googleusercontent.com",
         // googleSecretKey: "QSAUjcDpxbGP2IIB8KlWlJww"
+    },
+    prod: {
+        url: 'http://www.ajeitameulayout.com.br/',
+        port: 21107,
+        ambiente: 'PROD',
+        session: {
+            secret: 'teste',
+            resave: false,
+            saveUninitialized: false,
+            cookie: { secure: false }
+        },
+        database: {
+            host: 'mysql26-farm36.kinghost.net',
+            port: 3306,
+            user: 'ajeitameulayou',
+            password: 'bl0gh1st0r1a',
+            database: 'ajeitameulayou'
+        },
+        // email: {
+        //     user: 'teste@gmail.com',
+        //     pass: 'teste' 
+        // },
+        // googleID: "629679185123-d8q71lomop2gukdgtqp2klb3atujmlnd.apps.googleusercontent.com",
+        // googleSecretKey: "QSAUjcDpxbGP2IIB8KlWlJww"
     }
+
 }
 
 exports.get = function get(env) {
 
     if(env.toLowerCase() == "dev")
+        return config.dev;
+
+    if(env.toLowerCase() == "prod")
         return config.dev;
         
 }
